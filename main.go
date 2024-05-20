e72731d96fed6ca01482e824ca379d510e2f8e82
Gabriel Moura <gmoura96@icloud.com>
2024-05-20 15:09:55 -0300

    Graceful Shutdown

--- main.go
+++ main.go
@@ -11,8 +11,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"strings"
+	"syscall"
+	"time"
 )
 
 var (
@@ -188,7 +191,6 @@ func handleWebDAV(w http.ResponseWriter, r *http.Request) {
 	}
 
 	userDir := filepath.Join(*rootDirectory, user.Username)
-	log.Printf("Pasta do usuário: %s\n", userDir)
 	if _, err := os.Stat(userDir); os.IsNotExist(err) {
 		http.Error(w, "Pasta do usuário não encontrada", http.StatusNotFound)
 		return
@@ -212,10 +214,39 @@ func main() {
 	flag.Parse()
 
 	mux := http.NewServeMux()
-
 	mux.Handle("/dav/", BasicAuthMiddleware(http.HandlerFunc(handleWebDAV)))
 	mux.Handle("/admin/user", BearerAuthMiddleware(http.HandlerFunc(handleUserAdmin)))
 
-	fmt.Println("Servidor WebDAV iniciado em dav://localhost:8080/dav/")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	// Channel to listen for termination signals
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	// Start server in a goroutine
+	go func() {
+		fmt.Println("Servidor WebDAV iniciado em dav://localhost:8080/dav/")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Erro ao iniciar o servidor: %v\n", err)
+		}
+	}()
+
+	// Block until a signal is received
+	<-stop
+
+	fmt.Println("\nIniciando o desligamento gracioso do servidor...")
+
+	// Create a deadline for the shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Shutdown the server gracefully
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Erro ao desligar o servidor: %v\n", err)
+	}
+
+	fmt.Println("Servidor desligado com sucesso.")
 }
